0af6c7f24e580e96a565cb1fbdf2de0a009341c5
boran ertem <mboranertem@gmail.com>
2026-09-03 17:57:22 +0300

    PostgreSQL entegrasyonu: dotenv, docker-compose healthcheck

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
 from database import db
 from models.contact import Contact
 from routes.contact_routes import contact_bp
 
+load_dotenv()
+
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///rehber.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"postgresql://{os.getenv('POSTGRES_USER')}:"
+    f"{os.getenv('POSTGRES_PASSWORD')}@"
+    f"{os.getenv('POSTGRES_HOST')}:"
+    f"{os.getenv('POSTGRES_PORT')}/"
+    f"{os.getenv('POSTGRES_DB')}"
+)
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
 app.register_blueprint(contact_bp)
-
-print("Blueprint başarıyla kayıt edildi.")
-print(app.url_map)
-
 
 @app.route("/")
 def ana_sayfa():
@@ -22,7 +30,6 @@ def ana_sayfa():
 
 with app.app_context():
     db.create_all()
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
